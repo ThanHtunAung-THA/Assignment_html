d01bbf2ee39268b4f1e6101f3a749bbb5617115c
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-06-20 17:53:09 +0630

    modified img pop up

---
+++ assests/js/script.js
@@ -0,0 +1,10 @@
+$('#imageModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var title = button.data('title'); // Extract info from data-* attributes
+    var text = button.data('text');
+    var src = button.attr('src');
+    var modal = $(this);
+    modal.find('.modal-title').text(title);
+    modal.find('.modal-body img').attr('src', src);
+    modal.find('#imageModalText').text(text);
+  });
